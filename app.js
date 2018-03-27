d656010dd170a0d17f88c25810625db95ff1db84
Abid Hasan <abidhasan@gmail.com>
2018-03-26 20:10:30 -0600

    routing

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var express = require("express");
 var app = express();
 
 //TEMPORARY
-const campgrounds = [{name: "Salmon Creek", image: "url"},{name: "Salmon Creek 3", image: "url"},{name: "Salmon Creek 2", image: "url"}]
+const campgrounds = [
+  {id: 0, name: "Salmon Creek", image: "url"},
+  {id: 1, name: "Salmon Creek 3", image: "url"},
+  {id: 2, name: "Salmon Creek 2", image: "url"}
+]
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-  //show all landing pages
-  res.render('landing');
-})
 
 app.get("/campgrounds", function(req, res) {
   //show all campgrounds
-  res.render('campgrounds', {data: campgrounds})
+  // res.render('campgrounds', {data: campgrounds})
+  res.json(campgrounds);
 });
 
-app.listen(3000, function() {
+app.listen(3001, function() {
   console.log("Server running on port 3000.");
 });
