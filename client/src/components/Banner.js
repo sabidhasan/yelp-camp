0b3d45d90a3866ec719022f6c142d7f632c0f043
Abid Hasan <abidhasan@gmail.com>
2018-03-27 23:01:24 -0600

    updated banner styles

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Banner extends React.Component {
   render(props) {
     return (
-      <div>
+      <div className="banner">
         <h1>Welcome to YelpCamp!</h1>
         <h2>View our hand picked campgrounds from all over the country!</h2>
         <button className="addCampground">Add Campground</button>
