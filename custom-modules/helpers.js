1b68cebae3f29428ea8c484fec6d7ca8e4acfe23
Abid Hasan <abidhasan@gmail.com>
2018-04-13 18:58:48 -0600

    working on icons

---
+++ custom-modules/helpers.js
@@ -0,0 +1,178 @@
+quotes = [
+  "Time camping isn’t time spent, it’s invested",
+  "Away is a place where it’s not about the money you spend.It’s about the moments you share",
+  "Hike more, worry less",
+  "Oh darling, let's be adventurers",
+  "I felt my lungs inflate with the onrush of scenery. Air, mountains, tree, people. I thought: this is what it is to be happy",
+  "Life begins when you start saying 'Yes'",
+  "There is no Wi-fi in the forest, but I promise you will find a better connection",
+  "I am most alive among tall trees",
+  "Camping is “Home”",
+  "Wander often, wander always. because not all who wander are lost",
+  "Just GO!",
+  "Don’t let your dreams be dreams",
+  "There’s no place like camp. I wish I could stay forever",
+  "I don’t need Therapy. I just need to go camping",
+  "Explore.",
+  "The Mountains Are Calling And I Must Go",
+  "A Bonfire Is Basically Just A Nightclub In The Mountains",
+  "Live Laugh Camp",
+  "Be Brave Run Free And Stay Wild",
+  "Camping is the answer",
+  "Collect memories, not things",
+  "Life is better in the woods",
+  "Cold Air Dark Night Warm Fire Bright Stars",
+  "Camp more, worry less",
+  "Of all the paths you can take in life, make sure a few of them are dirt",
+  "The forest makes your heart gentle",
+  "Adopt the pace of nature: her secret is patience",
+  "Adventure is waiting",
+  "Born to camp",
+  "Nothing adventured, nothing attained",
+  "The earth has music for those who listen",
+  "Wander without a reason or purpose",
+  "Live in the sunshine, swim in the sea, drink the wild air",
+  "The poetry of the earth is never dead",
+  "I took a walk in the woods and came out taller than the trees",
+  "Take only memories, leave only footprints",
+  "Camping is nature's way of promoting the motel business",
+  "A crude meal, no doubt, but the best of all sauces is hunger.",
+  "A great many people, and more all the time, live their entire lives without ever once sleeping out under the stars",
+  "The stars were better company anyway. They were very beautiful, and they almost never snored.",
+  "“Forget not that the earth delights to feel your bare feet and winds long to play with your hair",
+  "Nature gives to every time and season some beauties of its own",
+  "Choose only one master – Nature",
+  "We do not see nature with our eyes, but with our understandings and our hearts"
+]
+
+
+
+
+// THIS IS KEYWORDS WITH NO PICTURE
+// IN BELOW OBJECT
+"cabins for rent (on-site)",
+"snack bar",
+"municipal park",
+"drugstore/pharmacy",
+"liquor store",
+"golf driving range",
+"hospital",
+"medical facilities",
+"restaurants",
+"fishing trips",
+"national park",
+"festivals",
+"attractions and services",
+"banking machine (atm)", 'banking machine'
+"theatres",
+"sports complex",
+"flea markets",
+"tennis courts"       'tennis court'
+"golf clubs rental",
+"grocery",
+"clubs/bars",
+"aquapark",
+"provincial park",
+"historical sites/museums",
+"racetrack",
+"sea-doo rental",
+"planned activities",
+
+
+
+"village shops",  'convenience store'
+"gasoline station",     'gasoline'
+"picnic shelters",    'picnic tables'
+'golf clubs rental'       'golf course'
+'village shops'               'convenience store'
+'aquapark'                    'water park'
+"poor",         DELETE
+"campground logo",    DELETE
+
+
+
+{
+  'playground'
+  'pavilion'
+  'picnic tables'
+  'shaded sites'
+  'volleyball court'
+    'pull thru sites' :            '🚛',
+    'full hookup sites' :          '🚛',
+    'tenting sites' :              '🎪',
+    'flush toilets' :              '🚽',
+    'security gate' :              '🚪',
+    'pets allowed' :               '🐕',
+    'hot showers' :                '🚿',
+    'internet access' :            '💻',
+    'firewood' :                   '🌳',
+    'seasonal sites' :             '🌞',
+    'restrooms' :                  '🚻',
+    'ice' :                        '◻️',
+    'beach swimming' :             '🏊',
+    'big rigs welcome' :           '🚚',
+    'motor boating' :              '🚤',
+    'hiking trails' :              '🗻',
+    'dumping station' :            '🚮',
+    'paddleboat rentals' :         '🚣',
+    'windsurfing' :                '🏄',
+    'horseshoes' :                 '🐎',
+    'marina' :                     '🚢',
+    'canoe rentals' :              '🚣',
+    'soccer field' :               '⚽',
+    'cycling trails' :             '🚴',
+    'kayaking' :                   '🚣',
+    'gasoline' :                   '⛽',
+    'paddleboats' :                '🚣',
+    'laundromat' :                 '👔',
+    'movie rentals' :              '🎬',
+    'canoeing' :                   '🚣',
+    'water skiing' :               '🎿',
+    'fishing' :                    '🎣',
+    'basketball court' :           '🏀',
+    'motorboat rentals' :          '🚤',
+    'campground photo' :           '📷',
+    'recreation room' :            '🎮',
+    'swimming pool' :              '🏊',
+    'mini-golf' :                  '⛳',
+    'golf course' :                '⛳',
+    'hot tub' :                    '🛁',
+    'fire pits' :                  '🔥',
+    'restaurant' :                 '🍴',
+    'convenience store' :          '🏪',
+    '50 amps sites' :              '🔌',
+    'planned activities' :         '📅',
+    'handicap access' :            '♿',
+    'dishwashing station' :        '🚿',
+    'rv supplies' :                '🏪',
+    'propane gas' :                '💨',
+    'baseball field' :             '⚾️',
+    'bicycle rentals' :            '🚲',
+    'group sites' :                '👪',
+    'wilderness sites' :           '🌻',
+    'spa/sauna' :                  '💆',
+    'pool table' :                 '🎱',
+    'video arcade' :               '📼',
+    'mountain climbing' :          '🌄',
+    'open all year' :              '📅',
+    'motocross trails' :           '🚲',
+    'barbebue rentals' :           '🍗',
+    'snack bar' :                  '🍩',
+    'horseback riding' :           '🏇',
+    'tennis court' :               '🎾',
+    'shuffleboard' :               '🔀',
+    'go-carts' :                   '🚗',
+    'mobile sewer service' :       '🚽',
+    'video player rentals' :       '📺',
+    'driving range' :              '⛳',
+    'banking machine' :            '💵',
+    'cable tv hookups' :           '📺',
+    'golf cart rentals' :          '⛳',
+    'summer camp' :                '🌞',
+    'rvs for rent (on-site)' :      '🚐',
+    'cabins for rent (on-site)' :   '🏠',
+    'shopping'                  :   '',
+    'casino'  :                       '🎲'
+    'children activities'       :   '👶'
+    'water park'              : '⛲'
+}
