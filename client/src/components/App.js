be38a812387ecebbe7e0ad35ea5bc4ecda8dec8b
Abid Hasan <abidhasan@gmail.com>
2018-04-16 21:09:09 -0600

    front end updates for consuming mongodb data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-
 import Header from './Header'
 import Banner from './Banner'
 import Campgrounds from './Campgrounds'
@@ -12,55 +11,35 @@ import SingleCampground from './SingleCampground'
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      campgrounds: [],
-      highlightedCampgrounds: [],
       quote: undefined
     }
 
-    this.addNewCampground = this.addNewCampground.bind(this);
-    this.newRandoms = this.newRandoms.bind(this);
+    // this.newRandoms = this.newRandoms.bind(this);
   }
 
-  addNewCampground(campground) {
-    this.setState({ campgrounds: campground })
-  }
-
-  newRandoms(number = 10) {
-    // return 'number' random from campgrounds array, or entire array if needed
-    // copy array, and check if request is too long
-    const campgrounds = this.state.campgrounds.slice();
-    if (number >= campgrounds.length || number <= 0) return campgrounds;
-
-    // taken keeps track of which is done
-    let taken = [];
-    let ret = [];
-    do {
-      //pick a new random
-      const elem = Math.floor(Math.random() * campgrounds.length);
-      //if it is a new campground, then add to ret array
-      if (taken.indexOf(elem) === -1) {
-        taken.push(elem);
-        ret.push(campgrounds[elem]);
-      }
-    } while (ret.length < number);
-    // return ret;
-    this.setState({ highlightedCampgrounds: ret })
-  }
+  // newRandoms() {
+  //   // Reset the state, then add 10 new campgrounds to it
+  //   this.setState({ campgrounds: [] }, () => {
+  //     for (var i = 0; i < 8; i++) {
+  //       fetch('/campground')
+  //         .then(res => res.json())
+  //         .then(cg => {
+  //           let x = this.state.campgrounds.slice();
+  //           x.push(cg)
+  //           this.setState({ campgrounds: x });
+  //       })
+  //     }
+  //   })
+  // }
 
   componentDidMount() {
-    fetch('/campground')
+    // Get quote
+    fetch('/quote')
       .then(res => res.json())
-      .then(campgrounds => {
-        this.setState({ campgrounds: campgrounds });
-        //gerate new highlighted campgrounds
-        this.newRandoms(8);
-      });
-
-      fetch('/quote')
-        .then(res => res.json())
-        .then(quote => this.setState({quote: quote[0]}));
-    }
+      .then(quote => this.setState({quote: quote[0]}));
+  }
 
   render() {
     return (
@@ -76,8 +55,8 @@ class App extends React.Component {
             return (
               <React.Fragment>
                 <Banner {...routerProps} quote={this.state.quote} />
-                <Campgrounds {...routerProps} campgrounds={this.state.highlightedCampgrounds}
-                  newRandoms={this.newRandoms}
+                <Campgrounds {...routerProps} //campgrounds={this.state.campgrounds}
+                  //newRandoms={this.newRandoms}
                 />
               </React.Fragment>
             );
--- client/src/components/Campgrounds.js
+++ client/src/components/Campgrounds.js
@@ -4,12 +4,25 @@ import CampgroundTile from './CampgroundTile'
 class Campgrounds extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {spin: false};
+    this.state = {
+      spin: false,
+      campgrounds: []
+    };
+
+    this.newRandoms = this.newRandoms.bind(this);
+  }
+
+  newRandoms() {
+    // Reset the state, then add 10 new campgrounds to it
+    fetch('/campground')
+      .then(res => res.json())
+      .then(cg => this.setState({ campgrounds: cg }))
   }
 
   componentDidMount() {
     this.spinner.addEventListener('click', () => this.setState({spin: true}));
     this.spinner.addEventListener('animationend', () => this.setState({spin: false}));
+    this.newRandoms();
   }
 
   render() {
@@ -18,16 +31,16 @@ class Campgrounds extends React.Component {
         <h1>
           <span>Some Awesome Campgrounds</span>
           <span
-            onClick={() => {this.props.newRandoms()}}
+            onClick={this.newRandoms}
             className={this.state.spin ? 'spin' : ''}
             ref={spinner => this.spinner = spinner}
             >
             🔃
           </span>
         </h1>
-        {this.props.campgrounds.map(val => {
+        {this.state.campgrounds.map(val => {
           return (
-            <li key={val.id}>
+            <li key={val['_id']}>
               <CampgroundTile id={val.id} name={val.name} image={val.image}/>
             </li>
           )})
