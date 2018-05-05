4ce4ffc42a90b95a473b4516c5c6f395bd6fb94f
Abid Hasan <abidhasan@gmail.com>
2018-05-05 12:47:12 -0600

    landing tiles

---
+++ client/src/components/LandingTiles.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const LandingTiles = () => {
+  return (
+    <div className='landing-text landing-tile'>
+      <section>
+        <i>Icon</i>
+        <h2>1550</h2>
+        <p>Campgrounds</p>
+      </section>
+
+      <section>
+        <i>Icon</i>
+        <h2>1</h2>
+        <p>Easy-to-use Source</p>
+      </section>
+
+      <section>
+        <i>Icon</i>
+        <h2>13</h2>
+        <p>Provinces and Territories</p>
+      </section>
+
+      <section>
+        <i>Icon</i>
+        <h2>1.09</h2>
+        <p>million Canadians camp</p>
+      </section>
+    </div>
+  )
+}
+
+export default LandingTiles
