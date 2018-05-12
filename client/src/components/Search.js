b13c6010fbf5e538f6c5a8246df097e77d730d3b
Abid Hasan <abidhasan@gmail.com>
2018-05-11 19:41:14 -0600

    Move map-related state to SearchResultsMap component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { parse } from 'query-string'
 import PropTypes from 'prop-types';
+import { parse } from 'query-string'
 import haversine from 'haversine'
 
 import { provinces, activitySymbols, shortenDescription } from '../helpers/helpers'
@@ -8,7 +8,7 @@ import SearchResultTile from './SearchResultTile'
 import SearchBar from './SearchBar'
 import FilterSearch from './FilterSearch'
 import Pagination from './Pagination'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import SearchResultsMap from './SearchResultsMap'
 
 class Search extends React.Component {
   constructor(props) {
@@ -23,10 +23,10 @@ class Search extends React.Component {
       page: 0,
       filterAreaExpanded: false,
       hovered: null,
-//      mapScroll: ''
+      selectBoxRegions: null,
+      selectBoxActivities: null
     };
     this.updateFilteredResults = this.updateFilteredResults.bind(this)
-//    this.shortenDescription = this.shortenDescription.bind(this)
     this.prevPage = this.prevPage.bind(this)
     this.nextPage = this.nextPage.bind(this)
     this.goToPage = this.goToPage.bind(this)
@@ -34,37 +34,37 @@ class Search extends React.Component {
   }
 
   // Holds location of the user if available
-  static contextTypes = {
-    userLocation: PropTypes.object,
-  }
+  static contextTypes = { userLocation: PropTypes.object };
 
   componentDidMount() {
     fetch(`/search?q=${this.searchQuery}`)
       .then(res => res.json())
       .then(search => {
-        search = search.map(v => {
-          const cgLoc = {latitude: v.lat, longitude: v.lon}
-          if (this.context.userLocation && this.context.userLocation.latitude && this.context.userLocation.longitude && v.lat && v.lon) {
-            v.distanceFromUser = Math.round(haversine(cgLoc, this.context.userLocation))
+        search = search.map(result => {
+          const cgLoc = {latitude: result.lat, longitude: result.lon}
+          if (this.context.userLocation && this.context.userLocation.latitude && this.context.userLocation.longitude && result.lat && result.lon) {
+            result.distanceFromUser = Math.round(haversine(cgLoc, this.context.userLocation))
           }
-          return v;
+          return result;
+        })
+
+        // Create activities for dropdown
+        const selectBoxRegions = []
+        let selectBoxActivities = []
+
+        search.forEach(result => {
+          const _item = result.region;
+          selectBoxActivities = selectBoxActivities.concat(result.activities);
+          if (!selectBoxRegions.includes(_item) && _item) selectBoxRegions.push(_item)
         });
-        this.setState({originalResults: search, filteredResults: search})
+
+        this.setState({
+          originalResults: search,
+          filteredResults: search,
+          selectBoxRegions: selectBoxRegions,
+          selectBoxActivities: Array.from(new Set(selectBoxActivities))
+        })
       })
-
-    // Add event listener for scroll for map
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 300 && this.state.mapScroll) return;
-      if (window.scrollY <= 300 && this.state.mapScroll === '') return;
-      let mapScrollClass = window.scrollY > 300 ? 'map-scrolling-fixed' : '';
-      this.setState({mapScroll: mapScrollClass})
-    })
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    // if (nextState.mapScroll === this.state.mapScroll) return false;
-    console.log(nextState);
-    return true;
   }
 
   prevPage(e) {
@@ -83,7 +83,7 @@ class Search extends React.Component {
   }
 
   toggleExpanded() {
-      this.setState({filterAreaExpanded: !this.state.filterAreaExpanded})
+    this.setState({filterAreaExpanded: !this.state.filterAreaExpanded})
   }
 
   updateFilteredResults(val) {
@@ -92,7 +92,6 @@ class Search extends React.Component {
     for (let item in this.state.originalResults) {
       // Filter based on province, region, activities
       const currItem = this.state.originalResults[item];
-
       if ((!val.selectedProv || provinces[val.selectedProv].toLowerCase() === currItem.province.toLowerCase()) &&
         (val.selectedRegions.includes(currItem.region) || !currItem.region) &&
         (!currItem.activities.length || currItem.activities.some(v => val.selectedActivities.includes(v)))
@@ -117,12 +116,7 @@ class Search extends React.Component {
       .slice(this.state.page * 10, (this.state.page * 10) + 10)
       .map((r, idx) => {
         return (
-          <li
-            key={r.id}
-            className='search-result'
-            // onMouseEnter={() => this.setState({hovered: r.id})}
-            // onMouseLeave={() => this.setState({hovered: null})}
-            >
+          <li key={r.id} className='search-result'>
             <SearchResultTile
               number={(this.state.page * 10) + idx + 1}
               name={r.campgroundName}
@@ -143,119 +137,46 @@ class Search extends React.Component {
     const currentResultStartUserFriendly = (this.state.page * 10) + 1;
     const currentResultEndUserFriendly = Math.min(((this.state.page * 10) + 10), this.state.filteredResults.length)
 
-    // Create activities for dropdown
-    const selectBoxRegions = []
-    let selectBoxActivities = []
+    const pagination = <Pagination currentPage={this.state.page + 1}
+      lastPage={Math.ceil(this.state.filteredResults.length / 10)}
+      totalResults={this.state.filteredResults.length} prevHandler={this.prevPage}
+      nextHandler={this.nextPage} goToPageHandler={this.goToPage} />
 
-    for (let item in this.state.originalResults) {
-      const _item = this.state.originalResults[item].region;
-      selectBoxActivities = selectBoxActivities.concat(this.state.originalResults[item].activities);
-      if (!selectBoxRegions.includes(_item) && _item) selectBoxRegions.push(_item)
-    }
-    selectBoxActivities = Array.from(new Set(selectBoxActivities))
-
-    // Markers for maps
-    let markers;
-    let bounds = new this.props.google.maps.LatLngBounds();
-    // const hoveredCG = this.state.filteredResults.find(v => v.id === this.state.hovered)
-    // if (this.state.hovered && hoveredCG.lat !== null && hoveredCG.lon !== null) {
-      // Generate one marker for hovered CG
-      // markers = [hoveredCG]
-    // } else {
-    markers = this.state.filteredResults
-      .slice(this.state.page * 10, (this.state.page * 10) + 10)
-    // }
-      .map((v, i) => {
-        if (v.lat && v.lon) {
-          bounds.extend({lat: v.lat, lng: v.lon});
-        }
-        const resultNum = (this.state.page * 10) + i + 1;
-        const color = v.id === this.state.hovered ? 'FFFFFF' : 'FF0000';
-        return (<Marker
-           key={i}
-           icon={{
-             url: `http://chart.apis.google.com/chart?chst=d_map_spin&chld=1|0|${color}|15|b|${resultNum}`,
-             scaledSize: new this.props.google.maps.Size(36,52)
-           }}
-           onClick={() => window.location = `/campground/${v.id}`}
-           position={{lat: v.lat, lng: v.lon }}>
-          </Marker>)
-    })
-
-
-
-    const pagination = (<Pagination currentPage={this.state.page + 1}
-              lastPage={Math.ceil(this.state.filteredResults.length / 10)}
-              totalResults={this.state.filteredResults.length} prevHandler={this.prevPage}
-              nextHandler={this.nextPage} goToPageHandler={this.goToPage} />)
-
-    // Holds center of the map
-    const initialCenter = {lat: (bounds.f.b + bounds.f.f)/2, lng: (bounds.b.b + bounds.b.f)/2}
-    const center = initialCenter;
-    // var zoom = 5;
-    // if (this.state.hovered) {
-    //     const cg =
-    //     console.log(bounds);
-    //     if (cg.lat !== null && cg.lon !== null) {
-    //       center = {lat: cg.lat, lng: cg.lon};
-    //       zoom = 12;
-    //       bounds = {north: cg.lon + 0.1, south: cg.lon - 0.1, east: cg.lat + 0.1, west: cg.lat - 0.1}
-    //     }
-    // }
     return (
       <div className='search-page-container'>
         <div className='filters'>
           <SearchBar initialValue={this.searchQuery} />
-
-          <h2>Filters</h2>
-          <p>Filter by...</p>
+          <h2>Filters</h2><p>Filter by...</p>
           <button onClick={this.toggleExpanded}>{this.state.filterAreaExpanded ? 'Hide ' : 'Show '}Filters</button>
-            <FilterSearch
-              provinces={this.selectBoxProvinces}
-              regions={selectBoxRegions}
-              activities={selectBoxActivities}
-              onChange={(val) => this.updateFilteredResults(val)}
-              className={this.state.filterAreaExpanded ? 'filter__section-expanded' : ''}
-            />
+          <FilterSearch
+            provinces={this.selectBoxProvinces}
+            regions={this.state.selectBoxRegions}
+            activities={this.state.selectBoxActivities}
+            onChange={(val) => this.updateFilteredResults(val)}
+            className={this.state.filterAreaExpanded ? 'filter__section-expanded' : ''}/>
         </div>
 
         {pagination}
 
         <div className='search-page-results'>
-            {markers.length > 0 ?
-            <div className={`discover__map google-map ${this.state.mapScroll}`}>
-              <Map
-                google={this.props.google}
-                // zoom={2}
-                // initialBounds={bounds}
-                initialCenter={initialCenter}
-                // center={center}
-                bounds={bounds}
-              >
-                {markers}
-              </Map>
-            </div>
-            : null}
+            {currentResultEndUserFriendly > 0
+              ? <h2>Showing results {currentResultStartUserFriendly}-{currentResultEndUserFriendly}
+                  {' '} of {this.state.filteredResults.length}
+                  {' '} for '<span className='bold'>{this.searchQuery}</span>'
+                </h2>
+              : null
+              }
+            <SearchResultsMap results={this.state.filteredResults} page={this.state.page}
+              hovered={this.state.hovered} />
+            <ol>
+              {results}
+            </ol>
 
-          {currentResultEndUserFriendly > 0
-            ? <h2>Showing results {currentResultStartUserFriendly}-{currentResultEndUserFriendly}
-                {' '} of {this.state.filteredResults.length}
-                {' '} for '<span className='bold'>{this.searchQuery}</span>'
-              </h2>
-            : null
-          }
-
-          <ol>
-            {results}
-          </ol>
-
-          {pagination}
+            {pagination}
+          </div>
         </div>
-      </div>
     )
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg'
-})(Search)
+export default Search
---
+++ client/src/components/SearchResultsMap.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+class SearchResultsMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mapScroll: '' };
+  }
+
+  componentDidMount() {
+    // Add event listener for scroll for map
+    window.addEventListener('scroll', () => {
+      let mapScrollClass = ''
+      if (window.scrollY > document.body.clientHeight - window.innerHeight - document.querySelector('footer').clientHeight) {
+        mapScrollClass = 'map-stick-bottom';
+      } else if (window.scrollY > 300) {
+        mapScrollClass = 'map-scrolling'
+      }
+      this.setState({mapScroll: mapScrollClass})
+    });
+    // Needed to make the map actually render
+    this.forceUpdate();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.state.mapScroll !== nextState.mapScroll || this.props != nextProps);
+  }
+
+  render() {
+    let bounds = new this.props.google.maps.LatLngBounds();
+    // Markers for maps
+    const markers = this.props.results
+      .slice(this.props.page * 10, (this.props.page * 10) + 10)
+      .map((v, i) => {
+        if (v.lat && v.lon) {
+          bounds.extend({lat: v.lat, lng: v.lon});
+        }
+        const resultNum = (this.props.page * 10) + i + 1;
+        const color = v.id === this.props.hovered ? 'FFFFFF' : 'FF0000';
+        return (
+          <Marker
+           key={i}
+           icon={{
+             url: `http://chart.apis.google.com/chart?chst=d_map_spin&chld=1|0|${color}|15|b|${resultNum}`,
+             scaledSize: new this.props.google.maps.Size(36,52)
+           }}
+           onClick={() => window.location = `/campground/${v.id}`}
+           position={{lat: v.lat, lng: v.lon }}>
+          </Marker>
+        )
+    })
+
+    // Holds center of the map
+    const initialCenter = {lat: (bounds.f.b + bounds.f.f)/2, lng: (bounds.b.b + bounds.b.f)/2}
+    if (markers.length === 0) return null;
+    return (
+      <div className={`discover__map google-map ${this.state.mapScroll}`}>
+        <Map
+          google={this.props.google}
+          initialCenter={initialCenter}
+          bounds={bounds}>
+          {markers}
+        </Map>
+      </div>
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg'
+})(SearchResultsMap)
