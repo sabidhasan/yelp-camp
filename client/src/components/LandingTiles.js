80d7a14fc8c37d8314af378545781e9c37f03d8c
Abid Hasan <abidhasan@gmail.com>
2018-05-14 19:36:09 -0600

    Bugfix: change class to className

--- client/src/components/LandingTiles.js
+++ client/src/components/LandingTiles.js
@@ -4,25 +4,25 @@ const LandingTiles = () => {
   return (
     <div className='landing-text landing-tile'>
       <section>
-        <i class="fas fa-home"></i>
+        <i className="fas fa-home"></i>
         <h2>1550</h2>
         <p>Unique Campgrounds</p>
       </section>
 
       <section>
-        <i class="far fa-bookmark"></i>
+        <i className="far fa-bookmark"></i>
         <h2>1</h2>
         <p>Easy-to-Use Source</p>
       </section>
 
       <section>
-        <i class="far fa-map"></i>
+        <i className="far fa-map"></i>
         <h2>12</h2>
         <p>Provinces and Territories</p>
       </section>
 
       <section>
-        <i class="fas fa-users"></i>
+        <i className="fas fa-users"></i>
         <h2>1.09</h2>
         <p>Million Canadians Camp Yearly</p>
       </section>
