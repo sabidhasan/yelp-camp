21f6290e0ad2686eaf8c8c02d725693c0abf4da2
Abid Hasan <abidhasan@gmail.com>
2018-08-20 23:12:00 -0600

    Add lazyloading for image on home page

--- client/src/components/DiscoverTile.js
+++ client/src/components/DiscoverTile.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import LazyLoad from 'react-lazyload';
 import { Link } from 'react-router-dom'
+import sunsetImage from '../images/test2.jpg'
 
 const DiscoverTile = (props) => {
   return (
     <div className='DiscoverTile flex-center'>
-      Find campgrounds in your province, and across Canada
+      <LazyLoad once>
+        <img className='DiscoverTile__img' src={sunsetImage} alt="Sunset over mountains"/>
+      </LazyLoad>
+      <span className='DiscoverTile__text'>
+        Find campgrounds in your province, and across Canada
+      </span>
       <Link
-        // onClick={() => window.location = '/discover'}
         to='/discover'
         className='DiscoverTile__button btn btn--large'
       >
