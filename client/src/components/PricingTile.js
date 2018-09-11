cdab84290cabc40a8b27170240facab6bafc5eab
Abid Hasan <abidhasan@gmail.com>
2018-09-10 22:19:06 -0600

    Fix bug in pricing format

--- client/src/components/PricingTile.js
+++ client/src/components/PricingTile.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const formatPricing = (pricing) => {
+  pricing = pricing.map(val => {
+    val = val.replace('$', '');
+    return val ? `$${val}` : ''
+  });
+  return pricing.join(' - ');
+}
+
 const PricingTile = (props) => (
   <React.Fragment>
     <h3>{props.type}</h3>
     <div className='Sidebar__text'>
-      {props.data.prices ? (props.data.prices[props.type] ? props.data.prices[props.type].join(' - $') : 'None')
+      {props.data.prices ? (props.data.prices[props.type] ? formatPricing(props.data.prices[props.type]) : 'None')
       : `No ${props.type} pricing provided`}
     </div>
   </React.Fragment>
